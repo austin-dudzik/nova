e6159250e686dc64b52da10e591cd7c778691a74
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-02-02 22:49:36 -0600

    Adjusted getVoters method to return multiple results

--- classes/Voters.php
+++ classes/Voters.php
@@ -14,9 +14,9 @@ class Voters
      * Returns >=5 random users who have upvoted a given post
      *
      * @param int $post_id The post ID
-     * @return Voters|Response|stdClass The voters or response object
+     * @return Voters|Response|array The voters or response object
      */
-    public static function getVoters(int $post_id): Voters|Response|stdClass
+    public static function getVoters(int $post_id): Voters|Response|array
     {
         global $conn;
         $stmt = $conn->prepare("SELECT CONCAT(us.first_name, ' ', us.last_name) name, CONCAT('https://gravatar.com/avatar/', md5(us.email), '?s=500') avatar FROM upvotes up LEFT JOIN users us ON up.user_id = us.id WHERE post_id = ? GROUP BY us.id ORDER BY RAND() LIMIT 5");
@@ -25,8 +25,18 @@ class Voters
         $result = $stmt->get_result();
 
         if ($result->num_rows > 0) {
+
+            // Define new array
+            $voters = array();
+
+            while ($voter = $result->fetch_object('Voters')) {
+                // Add voter to array
+                $voters[] = $voter;
+            }
+
             // Return random voters
-            return $result->fetch_object('Voters');
+            return $voters;
+
         } else {
             // Return 204 response
             return Response::throwResponse(204, "Data not found");
