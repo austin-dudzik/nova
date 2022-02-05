08e629870f5d32a6f05c7528f71d26788abb7f5d
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-02-05 00:12:51 -0600

    Re-formatted file

--- index.php
+++ index.php
@@ -8,37 +8,100 @@ include "includes/config.php";
 <html lang="en">
 <?php echo Render::header('Home'); ?>
 <body>
+<?php echo Render::navigation('feed'); ?>
 
-<?php include "includes/navigation.php"; ?>
-
-<div class="bg-dark text-white p-5">
+<div class="bg-primary text-white p-5"
+     sstyle="background:#a8138a">
     <div class="container p-5 py-3">
-        <h1>Your Feedback Matters to Us!</h1>
-        <p class="mb-4">We're here to build a better experience for you.</p>
-        <input type="search" class="form-control w-50" placeholder="Search for ideas, updates, users, and more...">
+        <h1 class="text-center">Your Feedback
+            Matters to Us!</h1>
+        <p class="mb-4 text-center">We're here to
+            build a
+            better experience for you.</p>
+
+        <style>
+            .input-icons .fa-magnifying-glass {
+                position: absolute;
+                padding: 17px 10px 10px 15px;
+                z-index: 999;
+            }
+
+            .search {
+                padding-top: 13px
+            }
+        </style>
+
+
+        <div class="input-icons input-group mb-3 w-50 mx-auto"
+             id="searchPageContainer">
+            <i class="far fa-magnifying-glass text-white"></i>
+            <input class="search form-control ps-5"
+                   type="text" id="searchPage"
+                   placeholder="Search for ideas, updates, users, and more...">
+        </div>
+
     </div>
 </div>
 
 <div class="container mt-5 px-5">
-    <h6 class="mb-4 mt-4"><svg xmlns="http://www.w3.org/2000/svg" class="d-inline me-2" style="height:20px;width:20px" viewBox="0 0 20 20" fill="currentColor">
-            <path d="M2 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1H3a1 1 0 01-1-1V4zM8 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1H9a1 1 0 01-1-1V4zM15 3a1 1 0 00-1 1v12a1 1 0 001 1h2a1 1 0 001-1V4a1 1 0 00-1-1h-2z" />
-        </svg> Boards</h6>
-    <div class="row mb-5" id="boards-container"></div>
+    <h6 class="mb-4 mt-4">
+        <svg xmlns="http://www.w3.org/2000/svg"
+             class="d-inline me-2"
+             style="height:20px;width:20px"
+             viewBox="0 0 20 20"
+             fill="currentColor">
+            <path d="M2 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1H3a1 1 0 01-1-1V4zM8 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1H9a1 1 0 01-1-1V4zM15 3a1 1 0 00-1 1v12a1 1 0 001 1h2a1 1 0 001-1V4a1 1 0 00-1-1h-2z"/>
+        </svg>
+        Boards
+    </h6>
+    <div class="row mb-5"
+         id="boards-container"></div>
 
     <h6 class="mb-4 mt-4">
 
-        <svg xmlns="http://www.w3.org/2000/svg" class="d-inline me-2" style="height:20px;width:20px" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M12 1.586l-4 4v12.828l4-4V1.586zM3.707 3.293A1 1 0 002 4v10a1 1 0 00.293.707L6 18.414V5.586L3.707 3.293zM17.707 5.293L14 1.586v12.828l2.293 2.293A1 1 0 0018 16V6a1 1 0 00-.293-.707z" clip-rule="evenodd" />
-        </svg> Roadmap</h6>
+        <svg xmlns="http://www.w3.org/2000/svg"
+             class="d-inline me-2"
+             style="height:20px;width:20px"
+             viewBox="0 0 20 20"
+             fill="currentColor">
+            <path fill-rule="evenodd"
+                  d="M12 1.586l-4 4v12.828l4-4V1.586zM3.707 3.293A1 1 0 002 4v10a1 1 0 00.293.707L6 18.414V5.586L3.707 3.293zM17.707 5.293L14 1.586v12.828l2.293 2.293A1 1 0 0018 16V6a1 1 0 00-.293-.707z"
+                  clip-rule="evenodd"/>
+        </svg>
+        Roadmap
+    </h6>
 
-    <div class="row mb-5" id="feed-container"></div>
+    <div class="row mb-5"
+         id="feed-container"></div>
 
 </div>
 
-<?php echo Render::footer(); ?>
+<!-- Modal -->
+<div class="modal fade mt-5" id="searchModal">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content bg-transparent border-0">
+            <div class="modal-body p-0">
+                <div class="input-icons input-group" id="searchInModal">
+                    <i class="far fa-magnifying-glass text-white"></i>
+                    <input type="text" class="search form-control ps-5" placeholder="Search for ideas, updates, users, and more...">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+><?php echo Render::footer(); ?>
 <script>
     const feedType = 1;
 </script>
 <script src="<?= $site_url ?>/assets/js/feed.js"></script>
+<script>
+    $(".input-icons input").on("focus", function () {
+        $(this).prev("i").toggleClass("text-white text-dark");
+    });
+    $(".input-icons input").on("blur", function () {
+        $(this).prev("i").toggleClass("text-white text-dark");
+    });
+</script>
 </body>
 </html>
