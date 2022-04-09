414e6a6362e32c32e248547079d8eee7391c1c95
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-04-08 22:48:17 -0500

    Updated getVoters method

--- classes/Voters.php
+++ classes/Voters.php
@@ -17,32 +17,41 @@ class Voters
      * @param int $limit The number of users to return
      * @return Voters|Response|array The voters or response object
      */
-    public static function getVoters(int $post_id, int $limit = 18446744073709551610): Voters|Response|array
+    public static function getVoters(int|null $post_id, int $limit = 1844674407370955161): Voters|Response|array
     {
         global $conn;
         global $prefix;
 
-        $stmt = $conn->prepare("SELECT CONCAT(us.first_name, ' ', us.last_name) name, CONCAT('https://gravatar.com/avatar/', md5(us.email), '?s=500') avatar FROM  ". $prefix . "upvotes up LEFT JOIN  ". $prefix . "users us ON up.user_id = us.id WHERE post_id = ? GROUP BY us.id LIMIT ?");
-        $stmt->bind_param("ii", $post_id, $limit);
-        $stmt->execute();
-        $result = $stmt->get_result();
+        $site_url = Settings::getSettings("site_url");
 
-        if ($result->num_rows > 0) {
+        if($post_id) {
 
-            // Define new array
-            $voters = [];
+            $stmt = $conn->prepare("SELECT CONCAT(us.first_name, ' ', us.last_name) name, CONCAT('https://gravatar.com/avatar/', md5(us.email), '?s=500&d=mp') avatar, CONCAT(?, '/u/', us.username) url FROM  " . $prefix . "upvotes up LEFT JOIN  " . $prefix . "users us ON up.user_id = us.id WHERE post_id = ? GROUP BY us.id LIMIT ?");
+            $stmt->bind_param("sii", $site_url, $post_id, $limit);
+            $stmt->execute();
+            $result = $stmt->get_result();
 
-            while ($voter = $result->fetch_object('Voters')) {
-                // Add voter to array
-                $voters[] = $voter;
+            if ($result->num_rows > 0) {
+
+                // Define new array
+                $voters = [];
+
+                while ($voter = $result->fetch_object('Voters')) {
+                    // Add voter to array
+                    $voters[] = $voter;
+                }
+
+                // Return random voters
+                return $voters;
+
+            } else {
+                // Return 204 response
+                return Response::throwResponse(204, "Data not found");
             }
 
-            // Return random voters
-            return $voters;
-
         } else {
-            // Return 204 response
-            return Response::throwResponse(204, "Data not found");
+            // Return 400 response
+            return Response::throwResponse(400, "Post ID is required for this request");
         }
 
     }
