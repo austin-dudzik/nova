0e2590853c90fe530f2977cd2738cdc28049f37a
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-04-13 17:47:36 -0500

    Updated getAllUsers method to filter user types

--- classes/Users.php
+++ classes/Users.php
@@ -29,12 +29,15 @@ class Users
      * @param string $user_slug The user slug
      * @return \Response The user or response object
      */
-    public static function getAllUsers(): array|Response
+    public static function getAllUsers(int $type = 0): array|Response
     {
         global $conn;
         global $prefix;
 
-        $stmt = $conn->prepare("SELECT us.id AS user_id, CONCAT(us.first_name , ' ', us.last_name) name, CONCAT('https://gravatar.com/avatar/', md5(us.email), '?s=500') avatar, us.username, (SELECT COUNT(po.id) FROM  " . $prefix . "posts po WHERE us.id = po.user_id) posts, (SELECT COUNT(up.id) FROM  " . $prefix . "upvotes up WHERE us.id = up.user_id) upvotes, us.created_at joined FROM  " . $prefix . "users us ORDER BY us.created_at DESC");
+        $site_url = Settings::getSettings("site_url");
+
+        $stmt = $conn->prepare("SELECT us.id AS user_id, CONCAT(us.first_name , ' ', us.last_name) name, CONCAT('https://gravatar.com/avatar/', md5(us.email), '?s=500&d=mp') avatar, CONCAT(?, '/u/', us.username) url, us.username, (SELECT COUNT(po.id) FROM  " . $prefix . "posts po WHERE us.id = po.user_id) posts, (SELECT COUNT(up.id) FROM  " . $prefix . "upvotes up WHERE us.id = up.user_id) upvotes, us.created_at joined FROM  " . $prefix . "users us WHERE is_admin =" . $type . " ORDER BY us.created_at DESC");
+        $stmt->bind_param("s", $site_url);
         $stmt->execute();
         $result = $stmt->get_result();
 
@@ -42,7 +45,7 @@ class Users
 
             $users = [];
 
-            while ($user = $result->fetch_object('Boards')) {
+            while ($user = $result->fetch_object('Users')) {
                 $users[] = $user;
             }
 
