fdbe133b7daec8fbc78556bfb9861c5cd32882ff
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-04-13 23:32:03 -0500

    Updated language file

--- lang/en.php
+++ lang/en.php
@@ -3,43 +3,41 @@
 /**
  * English translation
  *
- * Language file used in order to translate
+ * Language file used to translate
  * Nova application.
  */
 
-$lang = array();
-
-$lang['new_post'] = 'New Post';
-$lang['subscribe'] = 'Subscribe';
-$lang['subscribed'] = 'Subscribed';
-$lang['boards'] = 'Boards';
-$lang['options'] = 'Options';
-$lang['filter'] = 'Filter';
-$lang['sort'] = 'Sort';
-$lang['load_more'] = 'Load more';
-$lang['loading'] = 'Loading...';
-$lang['profile'] = 'Profile';
-$lang['logout'] = 'Log out';
-$lang['login'] = 'Log in';
-$lang['signup'] = 'Create account';
-$lang['search_text'] = 'Search for ideas, updates, users, and more...';
-$lang['feed'] = 'Feed';
-$lang['roadmap'] = 'Roadmap';
-$lang['changelog'] = 'Changelog';
-$lang['view_all'] = 'View all';
-$lang['post'] = 'post';
-$lang['posts'] = 'posts';
-$lang['upvote'] = 'upvote';
-$lang['upvotes'] = 'upvotes';
-$lang['nothing_here'] = 'Nothing here, yet. Check back soon!';
-$lang['readonly_mode'] = 'This feedback portal is currently in read-only mode and not accepting new ideas or upvotes.';
-$lang['email'] = 'Email';
-$lang['password'] = 'Password';
-$lang['welcome_back'] = 'Welcome back!';
-$lang['dont_have_account'] = 'Don\'t have an account?';
-$lang["board_not_found"] = 'Board Not Found';
-$lang["board_not_found_msg"] = 'Sorry, we couldn\'t find a board located at the specified URL.';
-$lang["not_found_reason"] = 'The page may have been moved, deleted, or may have never existed.';
-$lang["return_home"] = 'Go back home';
-$lang["reached_end"] = 'You\'ve reached the end';
-$lang["powered_by"] = 'Powered by';
+$lang = [
+    "new_post" => "New Post",
+    "boards" => "Boards",
+    "options" => "Options",
+    "filter" => "Filter",
+    "sort" => "Sort",
+    "load_more" => "Load more",
+    "loading" => "Loading...",
+    "profile" => "Profile",
+    "logout" => "Logout",
+    "login" => "Log in",
+    "signup" => "Signup",
+    "search_text" => "Search for ideas, updates, users, and more...",
+    "feed" => "Feed",
+    "roadmap" => "Roadmap",
+    "changelog" => "Changelog",
+    "view_all" => "View all",
+    "post" => "Post",
+    "posts" => "Posts",
+    "upvote" => "Upvote",
+    "upvotes" => "Upvotes",
+    "nothing_here" => "Nothing here, yet. Check back soon!",
+    "readonly_mode" => "This feedback portal is currently in read-only mode and not accepting new ideas or upvotes.",
+    "email" => "Email",
+    "password" => "Password",
+    "welcome_back" => "Welcome back!",
+    "dont_have_account" => "Don't have an account?",
+    "board_not_found" => "Board Not Found",
+    "board_not_found_msg" => "Sorry, we couldn't find a board located at the specified URL.",
+    "not_found_reason" => "The page may have been moved, deleted, or may have never existed.",
+    "return_home" => "Go back home",
+    "reached_end" => "You've reached the end",
+    "powered_by" => "Powered by",
+];
